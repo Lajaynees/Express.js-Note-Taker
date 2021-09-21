3de52d0ceadff7edcc5420cee8edb592caf5721a
Lajaynees <lscingram@yahoo.com>
2021-09-20 23:03:59 -0400

    add get.post

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
-
 const app = express();
 const PORT = process.env.PORT||3001;
+const { v4: uuidv4 } = require('uuid');
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 app.use(express.static('public'));
 
-
-
-
-//APP.GET-POST 
+//APP.GET-POST-POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note
 app.get('/api/notes', (req,res)=>{
     fs.readFile('./db/db.json',(err,data)=> {
         if (err) throw err
-        console.log (data)
+        const parsed = JSON.parse(data)
+        console.log (parsed)
+        res.json(parsed)
     })
+    app.post('/api/notes', (req) => {
+      const {title, text} = req.body
+      const newNote = {
+          title,
+          text,
+          id: uuidv4()
+      }
+     notes.push(newNote) 
 })
 
 app.get('/notes', (req, res) =>
@@ -30,5 +35,5 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
