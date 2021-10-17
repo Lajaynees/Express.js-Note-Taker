77a8c9c0bdef599fe23bf4e3db289731b177fc37
Lajaynees <lscingram@yahoo.com>
2021-10-17 14:26:33 -0400

    updated api routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const PORT = process.env.PORT||3001;
+const PORT = process.env.PORT || 3001;
 const { v4: uuidv4 } = require('uuid');
 
 // Middleware
@@ -11,21 +11,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //APP.GET-POST-POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note
-app.get('/api/notes', (req,res)=>{
-    fs.readFile('./db/db.json',(err,data)=> {
-        if (err) throw err
-        const parsed = JSON.parse(data)
-        console.log (parsed)
-        res.json(parsed)
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', (err, data) => {
+    if (err) throw err
+    const parsed = JSON.parse(data)
+    console.log(parsed)
+    res.json(parsed)
+  })
+})
+app.post('/api/notes', (req) => {
+  const { title, text } = req.body
+  const newNote = {
+    title,
+    text,
+    id: uuidv4()
+  }
+  fs.readFile('./db/db.json', (err, data) => {
+    if (err) throw err
+    const notes = JSON.parse(data)
+    notes.push(newNote)
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+      if (err) throw err
+    res.json(notes)
     })
-    app.post('/api/notes', (req) => {
-      const {title, text} = req.body
-      const newNote = {
-          title,
-          text,
-          id: uuidv4()
-      }
-     notes.push(newNote) 
+  })
+  
 })
 
 app.get('/notes', (req, res) =>
